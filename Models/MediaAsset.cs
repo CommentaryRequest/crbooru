e4e468a0785ba560409d3f89e0d4717e656049c1
CommentaryRequest <tele.didar.lol@gmail.com>
2026-09-20 17:02:41 +0500

    Add media asset variant type

--- Models/MediaAsset.cs
+++ Models/MediaAsset.cs
@@ -4,6 +4,13 @@ using System.IO;
 
 namespace CRbooru.Models;
 
+public enum MediaAssetVariantType
+{
+    Original,
+    Sample,
+    Thumbnail
+}
+
 [Index(nameof(Md5), IsUnique = true)]
 public class MediaAsset
 {
@@ -18,8 +25,14 @@ public class MediaAsset
         FileType = fileType;
     }
 
-    public string GetFilePath(string basePath)
+    public string GetFilePath(string basePath, MediaAssetVariantType variant)
     {
-        return Path.Combine(basePath, Md5[0..2], Md5[2..4], $"{Md5}.{FileType}");
+        string variantDir = variant switch
+        {
+            MediaAssetVariantType.Original => "original",
+            MediaAssetVariantType.Sample => "sample",
+            MediaAssetVariantType.Thumbnail => "thumb"
+        };
+        return Path.Combine(basePath, variantDir, Md5[0..2], Md5[2..4], $"{Md5}.{FileType}");
     }
 }
