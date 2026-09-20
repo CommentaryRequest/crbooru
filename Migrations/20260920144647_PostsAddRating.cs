25274ca536d3e86000f2510cdf6518b020ce775a
CommentaryRequest <tele.didar.lol@gmail.com>
2026-09-20 19:48:43 +0500

    Add ratings to posts

---
+++ Migrations/20260920144647_PostsAddRating.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRbooru.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostsAddRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Rating",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Posts");
+        }
+    }
+}
--- Models/Post.cs
+++ Models/Post.cs
@@ -3,12 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CRbooru.Models;
 
+public enum PostRating : byte
+{
+    General,
+    Sensitive,
+    Questionable,
+    Explicit
+}
+
 public class Post
 {
     public int Id { get; private set; }
     public User Uploader { get; private set; }
     public MediaAsset MediaAsset { get; set; }
     public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+    public PostRating Rating { get; set; } = PostRating.General;
 
     private Post()
     {
