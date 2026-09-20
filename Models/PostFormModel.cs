bbeac7894a4c4a7cf3e1e9c74ffaf1068b481aed
CommentaryRequest <tele.didar.lol@gmail.com>
2026-09-20 20:48:23 +0500

    Add source to post form model

--- Models/PostFormModel.cs
+++ Models/PostFormModel.cs
@@ -3,6 +3,7 @@ namespace CRbooru.Models;
 public class PostFormModel
 {
     public int MediaAssetId { get; set; }
+    public string Source { get; set; }
     public PostRating Rating { get; set; }
     public string TagString { get; set; }
 }
