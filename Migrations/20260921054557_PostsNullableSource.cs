306cf4589386bc12adfc9b7ad9dc2f52bf49c4e0
CommentaryRequest <tele.didar.lol@gmail.com>
2026-09-21 10:46:53 +0500

    Allow post source to be null

---
+++ Migrations/20260921054557_PostsNullableSource.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRbooru.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostsNullableSource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                table: "Posts",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
